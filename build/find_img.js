594ead298f442a37ab78caa813b94855486f500e
NiE-jA889 <aleksanderdymek0@gmail.com>
2026-07-13 20:16:39 +0200

    Delete build directory

--- build/find_img.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('galeria.html', 'utf8');
-const urls = [];
-const regex = /<img[^>]+src=["']([^"']+)["']/g;
-let match;
-while ((match = regex.exec(html)) !== null) {
-  urls.push(match[1]);
-}
-console.log(urls.slice(0, 15).join('\n'));
